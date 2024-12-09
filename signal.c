e13bf5f3506949bc3b571ef56472fb3097758d19
Ankur Gyawali <ankur.gyawali@bison.howard.edu>
2024-12-09 06:09:14 +0000

    New changes :final

--- signal.c
+++ signal.c
@@ -3,28 +3,40 @@
 #include <stdlib.h>
 #include <signal.h>
 #include <unistd.h>
+#include <stdbool.h>
+#include <time.h>
 
-volatile sig_atomic_t flag = 0;
+bool return_to_main = false;
+int begin;
 
-void handler(int signum)
-{
-    printf("Hello World!\n");
-    flag = 1;
+void handler(int signum) //signal handler
+{ 
+  printf("Hello World!\n");
+  return_to_main = true;
 }
 
-int main(int argc, char *argv[])
+void sig_int_handler(int signum){
+  clock_t end = clock();
+  double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+  printf("The total execution of time is: %f seconds.\n", time_spent);
+  exit(0);
+}
+
+int main(int argc, char * argv[])
 {
-    signal(SIGALRM, handler); // Register handler for SIGALRM
+  begin = clock();
+  signal(SIGINT, sig_int_handler);
+  signal(SIGALRM,handler); //register handler to handle SIGALRM
+  alarm(1);
 
-    while (1) {
-        alarm(5); // Set alarm for 5 seconds
-        flag = 0; // Reset flag
+  
+  while(1){ //loop indefinitely to print
+  if(return_to_main){ 
+    printf("Turing was right!\n");
+    return_to_main=false;
+    alarm(1);
+  }
+  };
 
-        // Busy wait for the signal to be delivered
-        while (!flag);
-
-        printf("Turing was right!\n");
-    }
-
-    return 0;
+  return 0; 
 }
